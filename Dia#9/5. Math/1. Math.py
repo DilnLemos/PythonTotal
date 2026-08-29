910bd8da4f84a670fcfd7df3a8b3aeafcdd59ac0
DilnLemos <dilan.lemos@correounivalle.edu.co>
2026-08-29 14:28:52 -0500

    Feat PythonTotal Dia 9, Librería Math

---
+++ Dia#9/5. Math/1. Math.py
@@ -0,0 +1,13 @@
+"""
+Módulo que contiene todo lo importante para operar con números
+desde lo básico hasta trigonometría y más.
+"""
+
+import math
+
+floor = math.floor(2.31) # Fucnión piso
+ceil = math.ceil(32.1) # Función techo
+pi = math.pi # Número pi
+log = math.log(100, 32) # Función logaritmica (resultado esperado, número a elevar)
+cos = math.cos(100) # Función coseno
+sin = math.sin(100) # Función seno
---
+++ Dia#9/5. Math/2.EjerciciosMath.py
@@ -0,0 +1,26 @@
+"""
+Obtén el logaritmo base 10 del número 25, y almacena el resultado en la variable resultado.
+
+Puedes utilizar el método math.log10()
+"""
+
+import math
+
+resultado = math.log10(25)
+
+"""
+Obten la raíz cuadrada de pi con la constante math.pi y el método math.sqrt()
+ Almacena el resultado obtenido en la variable resultado.
+"""
+
+pi = math.pi
+resultado = math.sqrt(pi)
+
+
+"""
+Encuentra el factorial de 7 y almacena el resultado en la variable resultado.
+
+El método a utilizar es factorial()
+"""
+
+resultado = math.factorial(7)
